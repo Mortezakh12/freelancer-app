349b3b965287910cece16f9e6137ccc0a76be495
Mortezakh12 <114151002+Mortezakh12@users.noreply.github.com>
2024-09-22 10:58:59 +0330

    "Added @tanstack/react-query dependency, created httpService.js, and updated SendOTPForm.jsx"

--- src/features/authentication/SendOTPForm.jsx
+++ src/features/authentication/SendOTPForm.jsx
@@ -3,10 +3,22 @@ import TextField from "../../ui/TextField";
 
 const SendOTPForm = () => {
   const [phoneNumber, setPhoneNumber] = useState("");
+
+  const sendOtpHandler = (e) => {
+    e.preventDefault();
+  };
+
   return (
-    <form className="space-y-8">
-      <TextField label="شماره موبایل" name="phoneNumber" value={phoneNumber} onChange={e=>setPhoneNumber(e.target.value)} />
-      <button className="btn btn--primary w-full " type="submit">ارسال کد تایید</button>
+    <form className="space-y-8" onSubmit={sendOtpHandler}>
+      <TextField
+        label="شماره موبایل"
+        name="phoneNumber"
+        value={phoneNumber}
+        onChange={(e) => setPhoneNumber(e.target.value)}
+      />
+      <button type="submit" className="btn btn--primary w-full " type="submit">
+        ارسال کد تایید
+      </button>
     </form>
   );
 };
