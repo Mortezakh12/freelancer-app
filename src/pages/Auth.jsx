eab7cfbda03245125ce7b2462177f38c8853039d
Mortezakh12 <114151002+Mortezakh12@users.noreply.github.com>
2024-09-22 10:45:28 +0330

    "Updated package-lock.json and package.json with new dependencies, and modified Auth.jsx to use CheckOTPForm instead of SendOTPForm"

--- src/pages/Auth.jsx
+++ src/pages/Auth.jsx
@@ -1,10 +1,14 @@
-import SendOTPForm from "../features/authentication/SendOTPForm";
+import CheckOTPForm from "../features/authentication/CheckOTPForm";
+// import SendOTPForm from "../features/authentication/SendOTPForm";
 
 const Auth = () => {
   return (
-    <div className="w-full sm:max-w-sm">
-      <SendOTPForm />
+   <div className="flex justify-center pt-10">
+     <div className="w-full sm:max-w-sm">
+      {/* <SendOTPForm /> */}
+      <CheckOTPForm/>
     </div>
+   </div>
   );
 };
 export default Auth;
