83fc9d03027c26d6455a08620e92c975da6f9dd6
Mortezakh12 <114151002+Mortezakh12@users.noreply.github.com>
2024-11-21 11:15:11 +0330

    "Refactor ProjectTable component to use Table and ProjectRow components, removing inline table markup and utility functions"

---
+++ src/features/projects/ProjectRow.jsx
@@ -0,0 +1,56 @@
+import { TbPencilMinus } from "react-icons/tb";
+import Table from "../../ui/Table";
+import toLocalDateShort from "../../utils/toLocalDateShort";
+import { toPersianNumbersWithComma } from "../../utils/toPersianNumbers";
+import truncateText from "../../utils/truncateText";
+import { HiOutlineTrash } from "react-icons/hi";
+import Modal from "../../ui/Modal";
+import { useState } from "react";
+
+function ProjectRow({ project, index }) {
+  const [isEditOpen, setIsEditOpen] = useState(false);
+  return (
+    <Table.Row key={project._id}>
+      <td>{index + 1}</td>
+      <td>{truncateText(project.title, 30)}</td>
+      <td>{project.category.title}</td>
+      <td>{toPersianNumbersWithComma(project.budget)}</td>
+      <td>{toLocalDateShort(project.deadline)}</td>
+      <td>
+        <div className="flex flex-wrap items-center gap-2 max-w-[200px]">
+          {project.tags.map((tag) => (
+            <span className="badge badge--secondary" key={tag}>
+              {tag}
+            </span>
+          ))}
+        </div>
+      </td>
+      <td>{project.freelancer?.name || "-"}</td>
+      <td>
+        {project.status === "OPEN" ? (
+          <span className="badge badge--success">باز</span>
+        ) : (
+          <span className="badge badge--danger">بسته</span>
+        )}
+      </td>
+      <div className="flex items-center gap-x-4">
+        <button onClick={()=>setIsEditOpen(true)}>
+          <TbPencilMinus className="w-5 h-5 text-primary-900" />
+        </button>
+        <Modal
+          onClose={() => {
+            setIsEditOpen(false);
+          }}
+          title="modal title"
+          open={isEditOpen}
+        >
+          this is modal...
+        </Modal>
+        <button>
+          <HiOutlineTrash className="w-5 h-5 text-error" />
+        </button>
+      </div>
+    </Table.Row>
+  );
+}
+export default ProjectRow;
